9c83b7aa652d9872fd90fbb28a74ab0ebf064446
Jônatas Rodrigues <jonatas.rodriguess@gmail.com>
2018-11-26 02:03:42 -0200

    [UPD] Ajuste do Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,15 @@ COPY . .
 
 RUN composer install
 
-ADD startup.sh / 
-CMD chmod +x /startup.sh && /startup.sh
+# ADD startup.sh /app 
+# RUN chmod +x /app/startup.sh && /app/startup.sh
+
+# RUN rm -r database/database.sqlite
+# RUN touch database/database.sqlite
+# RUN php artisan migrate
+# RUN php artisan db:seed
+
+CMD php -S 0.0.0.0:7777 -t public
 
 EXPOSE 7777
 
--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
-rm -r /app/database/database.sqlite
-touch /app/database/database.sqlite
-php artisan migrate
-php artisan db:seed
-php -S 0.0.0.0:7777 -t public
+# rm -r /app/database/database.sqlite
+# touch /app/database/database.sqlite
+# cd /app && php artisan migrate
+# cd /app && php artisan db:seed
+# cd /app && php -S 0.0.0.0:7777 -t public
+
+cd /app && php artisan migrate
+cd /app && php artisan db:seed
